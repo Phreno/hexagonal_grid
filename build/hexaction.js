120b06139531807229d7be91aa0d444fea51f173
K3rnelP4n1k <losgoomy@gmail.com>
2020-12-01 07:47:54 +0000

    un peu de propreté ?

--- build/hexaction.js
+++ build/hexaction.js
@@ -1,20 +1,21 @@
 "use strict";
-// export import DoubleOffsetCoord = require('./grid/double-offset-coord');
-// export import Hex = require('./grid/hex');
-// export import Layout = require('./grid/layout');
-// export import OffsetCoord = require('./grid/offset-coord');
-// export import Orientation = require('./grid/orientation');
-// export import Point = require('./grid/point');
 Object.defineProperty(exports, "__esModule", { value: true });
-const DoubleOffsetCoord = require("./grid/double-offset-coord");
-const Hex = require("./grid/hex");
-const Layout = require("./grid/layout");
-const Orientation = require("./grid/orientation");
-const Point = require("./grid/point");
-window.hexaction = {
-    DoubleOffsetCoord,
-    Hex,
-    Layout,
-    Orientation,
-    Point
-};
+exports.DoubleOffsetCoord = require("./grid/double-offset-coord");
+exports.Hex = require("./grid/hex");
+exports.Layout = require("./grid/layout");
+exports.OffsetCoord = require("./grid/offset-coord");
+exports.Orientation = require("./grid/orientation");
+exports.Point = require("./grid/point");
+// import DoubleOffsetCoord = require('./grid/double-offset-coord');
+// import Hex = require('./grid/hex');
+// import Layout = require('./grid/layout');
+// import OffsetCoord = require('./grid/offset-coord');
+// import Orientation = require('./grid/orientation');
+// import Point = require('./grid/point');
+// (window as any).hexaction = {
+//   DoubleOffsetCoord,
+//   Hex,
+//   Layout,
+//   Orientation,
+//   Point
+// }
--- src/hexaction.ts
+++ src/hexaction.ts
@@ -1,21 +1,21 @@
-// export import DoubleOffsetCoord = require('./grid/double-offset-coord');
-// export import Hex = require('./grid/hex');
-// export import Layout = require('./grid/layout');
-// export import OffsetCoord = require('./grid/offset-coord');
-// export import Orientation = require('./grid/orientation');
-// export import Point = require('./grid/point');
+export import DoubleOffsetCoord = require('./grid/double-offset-coord');
+export import Hex = require('./grid/hex');
+export import Layout = require('./grid/layout');
+export import OffsetCoord = require('./grid/offset-coord');
+export import Orientation = require('./grid/orientation');
+export import Point = require('./grid/point');
 
-import DoubleOffsetCoord = require('./grid/double-offset-coord');
-import Hex = require('./grid/hex');
-import Layout = require('./grid/layout');
-import OffsetCoord = require('./grid/offset-coord');
-import Orientation = require('./grid/orientation');
-import Point = require('./grid/point');
+// import DoubleOffsetCoord = require('./grid/double-offset-coord');
+// import Hex = require('./grid/hex');
+// import Layout = require('./grid/layout');
+// import OffsetCoord = require('./grid/offset-coord');
+// import Orientation = require('./grid/orientation');
+// import Point = require('./grid/point');
 
-(window as any).hexaction = {
-  DoubleOffsetCoord,
-  Hex,
-  Layout,
-  Orientation,
-  Point
-}
+// (window as any).hexaction = {
+//   DoubleOffsetCoord,
+//   Hex,
+//   Layout,
+//   Orientation,
+//   Point
+// }
