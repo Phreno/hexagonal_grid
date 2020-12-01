4a0d8e93b55174655ce883fbfedadd06e16484c7
K3rnelP4n1k <losgoomy@gmail.com>
2020-12-01 15:07:28 +0000

    mise à jour du livrable

--- demo/hexaction.js
+++ demo/hexaction.js
@@ -811,3 +811,465 @@ exports.Point = require("./grid/point"); // import DoubleOffsetCoord = require('
 
 },{"./grid/double-offset-coord":1,"./grid/hex":2,"./grid/layout":3,"./grid/offset-coord":4,"./grid/orientation":5,"./grid/point":6}]},{},[7])(7)
 });
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.hexaction = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Hex = void 0;
+class Hex {
+    constructor(q, r, s) {
+        this.q = q;
+        this.r = r;
+        this.s = s;
+        if (Math.round(q + r + s) !== 0)
+            throw Error("q + r + s must be 0");
+    }
+    add(b) {
+        return new Hex(this.q + b.q, this.r + b.r, this.s + b.s);
+    }
+    subtract(b) {
+        return new Hex(this.q - b.q, this.r - b.r, this.s - b.s);
+    }
+    scale(k) {
+        return new Hex(this.q * k, this.r * k, this.s * k);
+    }
+    rotateLeft() {
+        return new Hex(-this.s, -this.q, -this.r);
+    }
+    rotateRight() {
+        return new Hex(-this.r, -this.s, -this.q);
+    }
+    static direction(direction) {
+        return Hex.directions[direction];
+    }
+    neighbor(direction) {
+        return this.add(Hex.direction(direction));
+    }
+    diagonalNeighbor(direction) {
+        return this.add(Hex.diagonals[direction]);
+    }
+    len() {
+        return (Math.abs(this.q) + Math.abs(this.r) + Math.abs(this.s)) / 2;
+    }
+    distance(b) {
+        return this.subtract(b).len();
+    }
+    round() {
+        let qi = Math.round(this.q);
+        let ri = Math.round(this.r);
+        let si = Math.round(this.s);
+        const qDiff = Math.abs(qi - this.q);
+        const rDiff = Math.abs(ri - this.r);
+        const sDiff = Math.abs(si - this.s);
+        if (qDiff > rDiff && qDiff > sDiff) {
+            qi = -ri - si;
+        }
+        else if (rDiff > sDiff) {
+            ri = -qi - si;
+        }
+        else {
+            si = -qi - ri;
+        }
+        return new Hex(qi, ri, si);
+    }
+    lerp(b, t) {
+        return new Hex(this.q * (1.0 - t) + b.q * t, this.r * (1.0 - t) + b.r * t, this.s * (1.0 - t) + b.s * t);
+    }
+    linedraw(b) {
+        const N = this.distance(b);
+        const aNudge = new Hex(this.q + 1e-06, this.r + 1e-06, this.s - 2e-06);
+        const bNudge = new Hex(b.q + 1e-06, b.r + 1e-06, b.s - 2e-06);
+        const results = [];
+        const step = 1.0 / Math.max(N, 1);
+        for (let i = 0; i <= N; i++) {
+            results.push(aNudge.lerp(bNudge, step * i).round());
+        }
+        return results;
+    }
+}
+exports.Hex = Hex;
+Hex.directions = [new Hex(1, 0, -1), new Hex(1, -1, 0), new Hex(0, -1, 1), new Hex(-1, 0, 1), new Hex(-1, 1, 0), new Hex(0, 1, -1)];
+Hex.diagonals = [new Hex(2, -1, -1), new Hex(1, -2, 1), new Hex(-1, -1, 2), new Hex(-2, 1, 1), new Hex(-1, 2, -1), new Hex(1, 1, -2)];
+
+},{}],2:[function(require,module,exports){
+"use strict";
+// From http://www.redblobgames.com/grids/hexagons/implementation.html
+// Copyright 2015 Red Blob Games <redblobgames@gmail.com>
+// License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Hexaction = void 0;
+const hex_1 = require("./grid/hex");
+// This code is used to generate the diagrams on implementation.html
+/* global Hex, Layout, Point */
+class Hexaction {
+    drawHex(ctx, layout, hex) {
+        const corners = layout.polygonCorners(hex);
+        ctx.beginPath();
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 1;
+        ctx.moveTo(corners[5].x, corners[5].y);
+        for (let i = 0; i < 6; i++) {
+            ctx.lineTo(corners[i].x, corners[i].y);
+        }
+        ctx.stroke();
+    }
+    colorForHex(hex) {
+        // Match the color style used in the main article
+        if (hex.q === 0 && hex.r === 0 && hex.s === 0) {
+            return "hsl(0, 50%, 0%)";
+        }
+        else if (hex.q === 0) {
+            return "hsl(90, 70%, 35%)";
+        }
+        else if (hex.r === 0) {
+            return "hsl(200, 100%, 35%)";
+        }
+        else if (hex.s === 0) {
+            return "hsl(300, 40%, 50%)";
+        }
+        else {
+            return "hsl(0, 0%, 50%)";
+        }
+    }
+    drawHexLabel(ctx, layout, hex) {
+        const pointSize = Math.round(0.5 * Math.min(Math.abs(layout.size.x), Math.abs(layout.size.y)));
+        const center = layout.hexToPixel(hex);
+        ctx.fillStyle = this.colorForHex(hex);
+        ctx.font = `${pointSize}px sans-serif`;
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(hex.len() === 0 ? "q,r,s" : (hex.q + "," + hex.r + "," + hex.s), center.x, center.y);
+    }
+    permuteQRS(q, r, s) {
+        return new hex_1.Hex(q, r, s);
+    }
+    permuteSRQ(s, r, q) {
+        return new hex_1.Hex(q, r, s);
+    }
+    permuteSQR(s, q, r) {
+        return new hex_1.Hex(q, r, s);
+    }
+    permuteRQS(r, q, s) {
+        return new hex_1.Hex(q, r, s);
+    }
+    permuteRSQ(r, s, q) {
+        return new hex_1.Hex(q, r, s);
+    }
+    permuteQSR(q, s, r) {
+        return new hex_1.Hex(q, r, s);
+    }
+    shapeParallelogram(q1, r1, q2, r2, constructor) {
+        const hexes = [];
+        for (let q = q1; q <= q2; q++) {
+            for (let r = r1; r <= r2; r++) {
+                hexes.push(constructor(q, r, -q - r));
+            }
+        }
+        return hexes;
+    }
+    shapeTriangle1(size) {
+        const hexes = [];
+        for (let q = 0; q <= size; q++) {
+            for (let r = 0; r <= size - q; r++) {
+                hexes.push(new hex_1.Hex(q, r, -q - r));
+            }
+        }
+        return hexes;
+    }
+    shapeTriangle2(size) {
+        const hexes = [];
+        for (let q = 0; q <= size; q++) {
+            for (let r = size - q; r <= size; r++) {
+                hexes.push(new hex_1.Hex(q, r, -q - r));
+            }
+        }
+        return hexes;
+    }
+    shapeHexagon(size) {
+        const hexes = [];
+        for (let q = -size; q <= size; q++) {
+            const r1 = Math.max(-size, -q - size);
+            const r2 = Math.min(size, -q + size);
+            for (let r = r1; r <= r2; r++) {
+                hexes.push(new hex_1.Hex(q, r, -q - r));
+            }
+        }
+        return hexes;
+    }
+    shapeRectangle(w, h, constructor) {
+        const hexes = [];
+        const i1 = -Math.floor(w / 2);
+        const i2 = i1 + w;
+        const j1 = -Math.floor(h / 2);
+        const j2 = j1 + h;
+        for (let j = j1; j < j2; j++) {
+            const jOffset = -Math.floor(j / 2);
+            for (let i = i1 + jOffset; i < i2 + jOffset; i++) {
+                hexes.push(constructor(i, j, -i - j));
+            }
+        }
+        return hexes;
+    }
+    drawGrid(id, backgroundColor, withLabels, layout, hexes) {
+        const canvas = document.getElementById(id);
+        if (!canvas) {
+            return;
+        }
+        const ctx = canvas.getContext('2d');
+        if (!ctx) {
+            return;
+        }
+        const width = canvas.width;
+        const height = canvas.height;
+        if (window.devicePixelRatio && window.devicePixelRatio !== 1) {
+            canvas.width = width * window.devicePixelRatio;
+            canvas.height = height * window.devicePixelRatio;
+            ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+        }
+        if (hexes === undefined) {
+            hexes = this.shapeRectangle(15, 15, this.permuteQRS);
+        }
+        ctx.fillStyle = backgroundColor;
+        ctx.fillRect(0, 0, width, height);
+        ctx.translate(width / 2, height / 2);
+        hexes.forEach((hex) => {
+            this.drawHex(ctx, layout, hex);
+            if (withLabels)
+                this.drawHexLabel(ctx, layout, hex);
+        });
+    }
+}
+exports.Hexaction = Hexaction;
+
+},{"./grid/hex":1}]},{},[2])(2)
+});
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.hexaction = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Hex = void 0;
+class Hex {
+    constructor(q, r, s) {
+        this.q = q;
+        this.r = r;
+        this.s = s;
+        if (Math.round(q + r + s) !== 0)
+            throw Error("q + r + s must be 0");
+    }
+    add(b) {
+        return new Hex(this.q + b.q, this.r + b.r, this.s + b.s);
+    }
+    subtract(b) {
+        return new Hex(this.q - b.q, this.r - b.r, this.s - b.s);
+    }
+    scale(k) {
+        return new Hex(this.q * k, this.r * k, this.s * k);
+    }
+    rotateLeft() {
+        return new Hex(-this.s, -this.q, -this.r);
+    }
+    rotateRight() {
+        return new Hex(-this.r, -this.s, -this.q);
+    }
+    static direction(direction) {
+        return Hex.directions[direction];
+    }
+    neighbor(direction) {
+        return this.add(Hex.direction(direction));
+    }
+    diagonalNeighbor(direction) {
+        return this.add(Hex.diagonals[direction]);
+    }
+    len() {
+        return (Math.abs(this.q) + Math.abs(this.r) + Math.abs(this.s)) / 2;
+    }
+    distance(b) {
+        return this.subtract(b).len();
+    }
+    round() {
+        let qi = Math.round(this.q);
+        let ri = Math.round(this.r);
+        let si = Math.round(this.s);
+        const qDiff = Math.abs(qi - this.q);
+        const rDiff = Math.abs(ri - this.r);
+        const sDiff = Math.abs(si - this.s);
+        if (qDiff > rDiff && qDiff > sDiff) {
+            qi = -ri - si;
+        }
+        else if (rDiff > sDiff) {
+            ri = -qi - si;
+        }
+        else {
+            si = -qi - ri;
+        }
+        return new Hex(qi, ri, si);
+    }
+    lerp(b, t) {
+        return new Hex(this.q * (1.0 - t) + b.q * t, this.r * (1.0 - t) + b.r * t, this.s * (1.0 - t) + b.s * t);
+    }
+    linedraw(b) {
+        const N = this.distance(b);
+        const aNudge = new Hex(this.q + 1e-06, this.r + 1e-06, this.s - 2e-06);
+        const bNudge = new Hex(b.q + 1e-06, b.r + 1e-06, b.s - 2e-06);
+        const results = [];
+        const step = 1.0 / Math.max(N, 1);
+        for (let i = 0; i <= N; i++) {
+            results.push(aNudge.lerp(bNudge, step * i).round());
+        }
+        return results;
+    }
+}
+exports.Hex = Hex;
+Hex.directions = [new Hex(1, 0, -1), new Hex(1, -1, 0), new Hex(0, -1, 1), new Hex(-1, 0, 1), new Hex(-1, 1, 0), new Hex(0, 1, -1)];
+Hex.diagonals = [new Hex(2, -1, -1), new Hex(1, -2, 1), new Hex(-1, -1, 2), new Hex(-2, 1, 1), new Hex(-1, 2, -1), new Hex(1, 1, -2)];
+
+},{}],2:[function(require,module,exports){
+"use strict";
+// From http://www.redblobgames.com/grids/hexagons/implementation.html
+// Copyright 2015 Red Blob Games <redblobgames@gmail.com>
+// License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Hexaction = void 0;
+const hex_1 = require("./grid/hex");
+// This code is used to generate the diagrams on implementation.html
+/* global Hex, Layout, Point */
+class Hexaction {
+    drawHex(ctx, layout, hex) {
+        const corners = layout.polygonCorners(hex);
+        ctx.beginPath();
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 1;
+        ctx.moveTo(corners[5].x, corners[5].y);
+        for (let i = 0; i < 6; i++) {
+            ctx.lineTo(corners[i].x, corners[i].y);
+        }
+        ctx.stroke();
+    }
+    colorForHex(hex) {
+        // Match the color style used in the main article
+        if (hex.q === 0 && hex.r === 0 && hex.s === 0) {
+            return "hsl(0, 50%, 0%)";
+        }
+        else if (hex.q === 0) {
+            return "hsl(90, 70%, 35%)";
+        }
+        else if (hex.r === 0) {
+            return "hsl(200, 100%, 35%)";
+        }
+        else if (hex.s === 0) {
+            return "hsl(300, 40%, 50%)";
+        }
+        else {
+            return "hsl(0, 0%, 50%)";
+        }
+    }
+    drawHexLabel(ctx, layout, hex) {
+        const pointSize = Math.round(0.5 * Math.min(Math.abs(layout.size.x), Math.abs(layout.size.y)));
+        const center = layout.hexToPixel(hex);
+        ctx.fillStyle = this.colorForHex(hex);
+        ctx.font = `${pointSize}px sans-serif`;
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(hex.len() === 0 ? "q,r,s" : (hex.q + "," + hex.r + "," + hex.s), center.x, center.y);
+    }
+    permuteQRS(q, r, s) {
+        return new hex_1.Hex(q, r, s);
+    }
+    permuteSRQ(s, r, q) {
+        return new hex_1.Hex(q, r, s);
+    }
+    permuteSQR(s, q, r) {
+        return new hex_1.Hex(q, r, s);
+    }
+    permuteRQS(r, q, s) {
+        return new hex_1.Hex(q, r, s);
+    }
+    permuteRSQ(r, s, q) {
+        return new hex_1.Hex(q, r, s);
+    }
+    permuteQSR(q, s, r) {
+        return new hex_1.Hex(q, r, s);
+    }
+    shapeParallelogram(q1, r1, q2, r2, constructor) {
+        const hexes = [];
+        for (let q = q1; q <= q2; q++) {
+            for (let r = r1; r <= r2; r++) {
+                hexes.push(constructor(q, r, -q - r));
+            }
+        }
+        return hexes;
+    }
+    shapeTriangle1(size) {
+        const hexes = [];
+        for (let q = 0; q <= size; q++) {
+            for (let r = 0; r <= size - q; r++) {
+                hexes.push(new hex_1.Hex(q, r, -q - r));
+            }
+        }
+        return hexes;
+    }
+    shapeTriangle2(size) {
+        const hexes = [];
+        for (let q = 0; q <= size; q++) {
+            for (let r = size - q; r <= size; r++) {
+                hexes.push(new hex_1.Hex(q, r, -q - r));
+            }
+        }
+        return hexes;
+    }
+    shapeHexagon(size) {
+        const hexes = [];
+        for (let q = -size; q <= size; q++) {
+            const r1 = Math.max(-size, -q - size);
+            const r2 = Math.min(size, -q + size);
+            for (let r = r1; r <= r2; r++) {
+                hexes.push(new hex_1.Hex(q, r, -q - r));
+            }
+        }
+        return hexes;
+    }
+    shapeRectangle(w, h, constructor) {
+        const hexes = [];
+        const i1 = -Math.floor(w / 2);
+        const i2 = i1 + w;
+        const j1 = -Math.floor(h / 2);
+        const j2 = j1 + h;
+        for (let j = j1; j < j2; j++) {
+            const jOffset = -Math.floor(j / 2);
+            for (let i = i1 + jOffset; i < i2 + jOffset; i++) {
+                hexes.push(constructor(i, j, -i - j));
+            }
+        }
+        return hexes;
+    }
+    drawGrid(id, backgroundColor, withLabels, layout, hexes) {
+        const canvas = document.getElementById(id);
+        if (!canvas) {
+            return;
+        }
+        const ctx = canvas.getContext('2d');
+        if (!ctx) {
+            return;
+        }
+        const width = canvas.width;
+        const height = canvas.height;
+        if (window.devicePixelRatio && window.devicePixelRatio !== 1) {
+            canvas.width = width * window.devicePixelRatio;
+            canvas.height = height * window.devicePixelRatio;
+            ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+        }
+        if (hexes === undefined) {
+            hexes = this.shapeRectangle(15, 15, this.permuteQRS);
+        }
+        ctx.fillStyle = backgroundColor;
+        ctx.fillRect(0, 0, width, height);
+        ctx.translate(width / 2, height / 2);
+        hexes.forEach((hex) => {
+            this.drawHex(ctx, layout, hex);
+            if (withLabels)
+                this.drawHexLabel(ctx, layout, hex);
+        });
+    }
+}
+exports.Hexaction = Hexaction;
+
+},{"./grid/hex":1}]},{},[2])(2)
+});
