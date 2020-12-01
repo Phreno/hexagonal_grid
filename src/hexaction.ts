ed27ed904bd7d12f20c05dd26d5a54a8765a94dd
K3rnelP4n1k <losgoomy@gmail.com>
2020-12-01 10:32:07 +0000

    ajout de reference

--- src/hexaction.ts
+++ src/hexaction.ts
@@ -4,18 +4,3 @@ export import Layout = require('./grid/layout');
 export import OffsetCoord = require('./grid/offset-coord');
 export import Orientation = require('./grid/orientation');
 export import Point = require('./grid/point');
-
-// import DoubleOffsetCoord = require('./grid/double-offset-coord');
-// import Hex = require('./grid/hex');
-// import Layout = require('./grid/layout');
-// import OffsetCoord = require('./grid/offset-coord');
-// import Orientation = require('./grid/orientation');
-// import Point = require('./grid/point');
-
-// (window as any).hexaction = {
-//   DoubleOffsetCoord,
-//   Hex,
-//   Layout,
-//   Orientation,
-//   Point
-// }
