0375cb25e3363bfad35a58d98a3a6a212189284d
K3rnelP4n1k <losgoomy@gmail.com>
2020-12-02 09:28:36 +0000

    dev en cours

--- src/hexaction.ts
+++ src/hexaction.ts
@@ -2,8 +2,8 @@
 // Copyright 2015 Red Blob Games <redblobgames@gmail.com>
 // License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
 
-import { Layout } from "./grid/layout";
-import { Hex } from "./grid/hex";
+import { Layout } from "./grid/layout"
+import { Hex } from "./grid/hex"
 import { Point } from "./grid/point"
 import { Diagram } from "./grid/diagram"
 
